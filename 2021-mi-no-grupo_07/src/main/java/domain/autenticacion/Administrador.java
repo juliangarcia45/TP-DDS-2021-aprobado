4dc846f7f2367a47deaa02884fedeedb11dfed89
Julián García <49066529+juliangarcia45@users.noreply.github.com>
2021-05-04 21:28:54 -0300

    Add files via upload

---
+++ 2021-mi-no-grupo_07/src/main/java/domain/autenticacion/Administrador.java
@@ -0,0 +1,56 @@
+package domain.autenticacion;
+
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import domain.notificacion.Contacto;
+import domain.organizacion.Documento;
+
+import java.io.*;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Administrador extends Usuario{
+
+    public static Map<String, String> nombreMap = new HashMap<String, String>();
+
+    public Administrador(String usuario, String contrasenia, String nombre, String apellido, List<Contacto> mediosDeContacto, Documento documento, Date fechaNacimiento) {
+        super(usuario, contrasenia, nombre, apellido, mediosDeContacto, documento, fechaNacimiento);
+    }
+
+    public void agregarCaracteristica (String caracteristica , String valor){
+        nombreMap.put(caracteristica, valor);
+    }
+
+    public void agregarMapAlFile(FileOutputStream organizacionConfig){
+        try {
+            organizacionConfig = new FileOutputStream("src\\main\\java\\domain\\autenticacion\\configOrganizacion.ser");
+            ObjectOutputStream out = new ObjectOutputStream(organizacionConfig);
+            out.writeObject(nombreMap);
+            out.close();
+            organizacionConfig.close();
+            System.out.printf("Map guardado en configOrganizacion.ser correctamente!\n");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }public Map<String, String> leerMapDelFile(FileOutputStream organizacionConfig) {
+        Map<String, String> mapAux = null;
+        try {
+            FileInputStream fileIn = new FileInputStream("src\\main\\java\\domain\\autenticacion\\configOrganizacion.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            mapAux = (Map<String, String>) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+            return null;
+        } catch (ClassNotFoundException c) {
+            System.out.println("error");
+            c.printStackTrace();
+            return null;
+        }
+        return mapAux;
+    }
+}
